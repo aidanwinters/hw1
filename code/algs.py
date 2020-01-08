e2e78f1e35046092a6195ef6c46b144efa9996b7
aidanwinters <aidanwinters95@gmail.com>
2020-01-08 14:34:56 -0800

    added tests in for bubblesort

--- code/algs.py
+++ code/algs.py
@@ -1,24 +1,47 @@
 import numpy as np
 
-def pointless_sort(x):
+def pointlessSort(x):
     """
     This function always returns the same values to show how testing
     works, check out the `test/test_alg.py` file to see.
     """
     return np.array([1,2,3])
 
-def bubblesort(x):
-    """
-    Describe how you are sorting `x`
-    """
+def bubbleSort(arr):
+    if len(arr) <= 1: #if the array is empty or only one element, return
+        return arr
 
-    assert 1 == 1
-    return x
+    for i in range(len(arr)):
+        for j in range((len(arr) - 1 - i)):
+            first = arr[j]
 
-def quicksort(x):
-    """
-    Describe how you are sorting `x`
-    """
+            if first > arr[j+1]:
+                arr[j] = arr[j+1]
+                arr[j+1] = first
+    return arr
 
-    assert 1 == 1
-    return
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low
+    for x in range(low, high):
+        if arr[x] < pivot:
+            arr[i],arr[x] = arr[x],arr[i]
+            i = i + 1
+
+    arr[i], arr[high] = arr[high], arr[i]
+
+    return i
+
+def quickSort_rec(arr, low, high):
+
+    if low >= high:
+        return
+    else:
+        p = partition(arr, low, high)
+        quickSort_rec(arr,low,p-1)
+        quickSort_rec(arr,p+1,high)
+
+    return arr
+
+def quickSort(arr):
+    return quickSort_rec(arr, 0, len(arr) - 1)
--- test/test_algs.py
+++ test/test_algs.py
@@ -6,29 +6,29 @@ def test_pointless_sort():
     x = np.random.rand(10)
 
     # check that pointless_sort always returns [1,2,3]
-    assert np.array_equal(algs.pointless_sort(x), np.array([1,2,3]))
+    assert np.array_equal(algs.pointlessSort(x), np.array([1,2,3]))
 
     # generate a new random vector of length 10
     x = np.random.rand(10)
 
     # check that pointless_sort still returns [1,2,3]
-    assert np.array_equal(algs.pointless_sort(x), np.array([1,2,3]))
+    assert np.array_equal(algs.pointlessSort(x), np.array([1,2,3]))
 
 def test_bubblesort():
-    # Actually test bubblesort here. It might be useful to think about
-    # some edge cases for your code, where it might fail. Some things to
-    # think about: (1) does your code handle 0-element arrays without
-    # failing, (2) does your code handle characters?
 
     x = np.array([1,2,4,0,1])
-    
-    # for now, just attempt to call the bubblesort function, should
-    # actually check output
-    algs.bubblesort(x)
+
+    assert np.array_equal(algs.bubbleSort([]), []) #EMPTY ARRAY
+    assert np.array_equal(algs.bubbleSort([1]), [1]) #ARRAY OF ONE
+    assert np.array_equal(algs.bubbleSort(x), sorted(x)) #SORT ACTUAL ARRAY
+
+    x = np.array([1,1,1,1,1])
+    assert np.array_equal(algs.bubbleSort(x), sorted(x)) #TEST DUPLICATES
+
 
 def test_quicksort():
 
     x = np.array([1,2,4,0,1])
     # for now, just attempt to call the quicksort function, should
     # actually check output
-    algs.quicksort(x)
+    algs.quickSort(x)
